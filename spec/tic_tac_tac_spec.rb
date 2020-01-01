34953402e17e197b098e855c254a9b6eb3b2870d
Muhammad Tashfeen <raotashfeen25@gmail.com>
2019-12-31 20:13:38 -0600

    start working on move method

--- spec/tic_tac_tac_spec.rb
+++ spec/tic_tac_tac_spec.rb
@@ -6,11 +6,20 @@ require './lib/player'
 
 RSpec.describe 'Board' do
   let(:board) { Board.new }
+  let(:player) { Player.new }
 
   describe '#input_to_index' do
     it 'input_to_index will return integer' do
       input = '1'
       expect(board.input_to_index(input)).to be_a(Integer)
+    end
+  end
+
+  describe '#move' do
+    it 'move will add current X or O in board' do
+      brd = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+      p = player.current_player(brd, player)
+      expect(board.move(brd, 0, p).to(eql([p.to_s, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])))
     end
   end
 end
