aba2f1326782cd2ecd19a4cb63eb342c7369472c
Qoosim <ayinde.abdulghaniyy@gmail.com>
2020-01-06 17:35:37 +0100

    commented lines deleted

--- spec/tic_tac_tac_spec.rb
+++ spec/tic_tac_tac_spec.rb
@@ -7,19 +7,24 @@ require './lib/player'
 RSpec.describe 'Board' do
   let(:board) { Board.new }
   let(:player) { Player.new }
+  let(:game_over) { GameStatus.new }
 
   describe '#input_to_index' do
-    it 'input_to_index will return integer' do
+    it 'converts the user-input to integer' do
       input = '1'
       expect(board.input_to_index(input)).to be_a(Integer)
     end
   end
 
-  describe '#move' do
-    it 'move will add current X or O in board' do
-      brd = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-      p = player.current_player(brd, player)
-      expect(board.move(brd, 0, p).to(eql([p.to_s, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '])))
+  describe '#game_over' do
+    context 'checks if there similar item on the row' do
+      it 'returns true if there are' do
+
+        brd = ['X', 'X', 'X', ' ', ' ', ' ', ' ', ' ', ' ']
+      
+        expect(game_over.game_over?(brd, [0, 1, 2])).to eql(true)
+      end
     end
   end
+
 end
