8910dcbc9280c2267ea5fe65ce320f6972d2239f
Muhammad Tashfeen <raotashfeen25@gmail.com>
2020-01-06 16:42:22 -0600

    turn count method is tested

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -21,4 +21,15 @@ RSpec.describe 'Player' do
       expect(plyr.current_player(brd, plyr)).to eql('O')
     end
   end
+
+  describe '#turn_count' do
+  it 'will gives No of Tokens are in board' do
+    brd = ['X', 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    expect(plyr.turn_count(brd)).to eql(2)
+  end
+  it 'will gives Number of Tokens are in board' do
+    brd = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    expect(plyr.turn_count(brd)).to eql(0)
+  end
+end
 end
